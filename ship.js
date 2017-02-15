ff701ceb00f1e5a95b5777d0f064add0580ac734
James Wilson <jbwilson1982@gmail.com>
2017-02-15 14:48:11 -0600

    can place ships on board

--- ship.js
+++ ship.js
@@ -14,22 +14,163 @@ var createGrid = function() {
 
 $('#board').append(createGrid());
 
-$('.cell').on('click', function(e) {
-  console.log("x: " + this.dataset.x + " y: " + this.dataset.y)
-})
-
-$("#patrol_boat").on("click", function(){
+$("#destroyer").on("click", function(){
   $('.cell').on('mouseover', function(e) {
     var boatLength = 2;
-    var x = this.dataset.x
-    $(this).css('background-color', 'gray');
-    var $next = $('td[data-x=' + x + '][data-y=' + (parseInt(this.dataset.y) + 1) + ']')
-    $next.css('background-color', 'gray');
+    var x = this.dataset.x;
+    var y = parseInt(this.dataset.y);
+    $(this).addClass("phantom");
+    var $next = $('td[data-x=' + x + '][data-y=' + (y + 1) + ']')
+    $next.addClass("phantom");
   })
   $('.cell').on('mouseleave', function(e) {
-    $(this).css('background-color', 'white');
+    $(this).removeClass("phantom");
     var x = this.dataset.x
-    var $next = $('td[data-x=' + x + '][data-y=' + (parseInt(this.dataset.y) + 1) + ']')
-    $next.css('background-color', 'white');
+    var y = parseInt(this.dataset.y)
+    var $next = $('td[data-x=' + x + '][data-y=' + (y + 1) + ']')
+    $next.removeClass("phantom");
   })
+  $('.cell').on('click', function(e) {
+    var x = this.dataset.x
+    var y = parseInt(this.dataset.y)
+    $(".cell").off('mouseover');
+    $(".cell").off('mouseleave');
+    $(this).addClass("ship")
+    $('td[data-x=' + x + '][data-y=' + (y + 1) + ']').css("background-color", "").addClass("ship").removeClass('phantom');
+    $("#destroyer").hide();
+    $(this).removeClass('phantom');
+
+  });
 });
+
+$("#submarine").on("click", function(){
+  var boatLength = 3;
+  $('.cell').on('mouseover', function(e) {
+    var x = this.dataset.x
+    var y = parseInt(this.dataset.y)
+    $(this).addClass("phantom");
+    for (var i=1; i < boatLength; i++ ){
+      $('td[data-x=' + x + '][data-y=' + (y + i) + ']').addClass("phantom");
+      }
+  })
+  $('.cell').on('mouseleave', function(e) {
+    $(this).removeClass("phantom");
+    var x = this.dataset.x
+    var y = parseInt(this.dataset.y)
+    for (i=1; i<boatLength; i++){
+      $('td[data-x=' + x + '][data-y=' + (y + i) + ']').removeClass("phantom");
+    }
+  })
+  $('.cell').on('click', function(e) {
+    var x = this.dataset.x
+    var y = parseInt(this.dataset.y)
+    $(".cell").off('mouseover');
+    $(".cell").off('mouseleave');
+    $(this).addClass("ship")
+     for(i=1; i<boatLength; i++){
+      $('td[data-x=' + x + '][data-y=' + (y + i) + ']').addClass("ship").removeClass('phantom');
+    }
+    $("#submarine").hide();
+    $(this).removeClass('phantom');
+  });
+});
+
+$("#cruiser").on("click", function(){
+  var boatLength = 3;
+  $('.cell').on('mouseover', function(e) {
+    var x = this.dataset.x
+    var y = parseInt(this.dataset.y)
+    $(this).addClass("phantom");
+    for (var i=1; i < boatLength; i++ ){
+      $('td[data-x=' + x + '][data-y=' + (y + i) + ']').addClass("phantom");
+      }
+  })
+  $('.cell').on('mouseleave', function(e) {
+    $(this).removeClass("phantom");
+    var x = this.dataset.x
+    var y = parseInt(this.dataset.y)
+    for (i=1; i<boatLength; i++){
+      $('td[data-x=' + x + '][data-y=' + (y + i) + ']').removeClass("phantom");
+    }
+  })
+  $('.cell').on('click', function(e) {
+    var x = this.dataset.x
+    var y = parseInt(this.dataset.y)
+    $(".cell").off('mouseover');
+    $(".cell").off('mouseleave');
+    $(this).addClass("ship")
+     for(i=1; i<boatLength; i++){
+      $('td[data-x=' + x + '][data-y=' + (y + i) + ']').addClass("ship").removeClass('phantom');
+    }
+    $("#cruiser").hide();
+    $(this).removeClass('phantom');
+  });
+});
+
+$("#battleship").on("click", function(){
+  var boatLength = 4;
+  $('.cell').on('mouseover', function(e) {
+    var x = this.dataset.x
+    var y = parseInt(this.dataset.y)
+    $(this).addClass("phantom");
+    for (var i=1; i < boatLength; i++ ){
+      $('td[data-x=' + x + '][data-y=' + (y + i) + ']').addClass("phantom");
+      }
+  })
+  $('.cell').on('mouseleave', function(e) {
+    $(this).removeClass("phantom");
+    var x = this.dataset.x
+    var y = parseInt(this.dataset.y)
+    for (i=1; i<boatLength; i++){
+      $('td[data-x=' + x + '][data-y=' + (y + i) + ']').removeClass("phantom");
+    }
+  })
+  $('.cell').on('click', function(e) {
+    var x = this.dataset.x
+    var y = parseInt(this.dataset.y)
+    $(".cell").off('mouseover');
+    $(".cell").off('mouseleave');
+    $(this).addClass("ship")
+    for(i=1; i<boatLength; i++){
+      $('td[data-x=' + x + '][data-y=' + (y + i) + ']').addClass("ship").removeClass('phantom');
+    }
+    $("#battleship").hide();
+    $(this).removeClass('phantom');
+  });
+});
+
+$("#carrier").on("click", function(){
+    var boatLength = 5;
+  $('.cell').on('mouseover', function(e) {
+    var x = this.dataset.x
+    var y = parseInt(this.dataset.y)
+    $(this).addClass("phantom");
+    for (var i=1; i < boatLength; i++ ){
+    $('td[data-x=' + x + '][data-y=' + (y + i) + ']').addClass("phantom");
+  }
+  })
+  $('.cell').on('mouseleave', function(e) {
+    $(this).removeClass("phantom");
+    var x = this.dataset.x
+    var y = parseInt(this.dataset.y)
+    for (i=1; i<boatLength; i++){
+      $('td[data-x=' + x + '][data-y=' + (y + i) + ']').removeClass("phantom");
+    }
+  })
+  $('.cell').on('click', function(e) {
+    var x = this.dataset.x
+    var y = parseInt(this.dataset.y)
+    $(".cell").off('mouseover');
+    $(".cell").off('mouseleave');
+    $(this).addClass("ship")
+    for(i=1; i<boatLength; i++){
+      $('td[data-x=' + x + '][data-y=' + (y + i) + ']').addClass("ship").removeClass('phantom');
+    }
+    $("#carrier").hide();
+    $(this).removeClass('phantom');
+  });
+});
+
+
+
+
